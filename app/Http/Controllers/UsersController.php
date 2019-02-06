a8fd224a57a94c73bf44cc8d8af167253813f9f5
张大美 <zhangdamei@zhangdameideMacBook-Pro.local>
2019-02-06 18:45:49 +0800

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
 
 use Illuminate\Http\Request;
 
@@ -9,5 +10,9 @@ class UsersController extends Controller
     public function create()
     {
         return view('users.create');
+    }
+    public function show(User $user)
+    { 
+	return view('users.show',compact('user'));
     }
 }
