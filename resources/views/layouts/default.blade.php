9ce6a477e97c020b829d75e37919a39f5d130c6e
Damei Zhang <damei.zhang@me.com>
2019-02-10 14:30:11 +0800

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,7 +9,8 @@
 
     <div class="container">
       <div class="offset-md-1 col-md-10">
-        @yield('content')
+        @include('shared._messages')
+	@yield('content')
         @include('layouts._footer')
       </div>
     </div>
